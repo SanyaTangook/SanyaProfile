c4880d0ef389bc7c4d87853f3dbd390ae6e2d532
SanyaTangook <93518732+SanyaTangook@users.noreply.github.com>
2024-02-21 10:14:55 +0700

    add btn

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function App() {
@@ -8,7 +9,7 @@ export default function App() {
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <Link className="btn btn-primary">Get Started</Link>
     </div>
   </div>
 </div>
